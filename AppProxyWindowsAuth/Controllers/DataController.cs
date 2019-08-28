892733b5f4288a13f1adfde45d214a3a326f020e
Anton Staykov <anstayk@microsoft.com>
2019-08-28 14:49:40 +0200

    upgrade + web api + CORS

---
+++ AppProxyWindowsAuth/Controllers/DataController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace AppProxyWindowsAuth.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class DataController : ApiController
+    {
+        // GET: api/Data
+        public IEnumerable<string> Get()
+        {
+            return new string[] {
+                "You have successfully called an API"
+                , "And the API sees you as: " + User.Identity.Name
+                , "And your authentication is: " + User.Identity.AuthenticationType
+            };
+        }
+
+   }
+}
